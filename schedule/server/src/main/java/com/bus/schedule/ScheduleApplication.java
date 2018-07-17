7945b6b6305e722fa05388cfee04a3e97ecd521a
zzylovecode <632893423@qq.com>
2018-07-17 19:05:27 +0800

    schedule

---
+++ schedule/server/src/main/java/com/bus/schedule/ScheduleApplication.java
@@ -0,0 +1,14 @@
+package com.bus.schedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleApplication.class, args);
+	}
+}
